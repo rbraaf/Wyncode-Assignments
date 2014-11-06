09166b39c29f4813418830ef58d43e2f6c39f096
Ray Braaf <braaf.raymond@gmail.com>
2014-11-06 16:37:47 -0500

    add stock ticker

---
+++ stock_ticker.rb
@@ -0,0 +1,26 @@
+require 'httparty'
+require 'nokogiri'
+
+
+loop do
+  puts "Please enter stock symbol"
+  stock = gets.chomp.downcase
+
+
+  response = HTTParty.get ("http://finance.yahoo.com/q?s=#{stock}")
+
+
+  dom = Nokogiri::HTML(response.body)
+
+  stock_price = dom.xpath("//span[@id='yfs_l84_#{stock}']").first
+  last_price = dom.xpath("//td [@class='yfnc_tabledata1']").first
+  days_range_low = dom.xpath("//span[@id='yfs_g53_#{stock}']").first
+  days_range_hi = dom.xpath("//span[@id='yfs_h53_#{stock}']").first
+
+  puts "=================================================="
+  puts "---------------------#{stock}-------------------------".upcase
+  puts "=================================================="
+  puts "$#{stock_price.content}"
+  puts "Yesterday's closing price was #{last_price.content}"
+  puts "Day's Range #{days_range_low.content} - #{days_range_hi.content}"
+end
