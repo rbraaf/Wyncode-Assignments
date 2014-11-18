97fcd67947263ad3da304daa738cf313fac4a04b
Ray Braaf <braaf.raymond@gmail.com>
2014-11-18 09:49:41 -0500

    more db updates

--- to-do_c3/db/schema.rb
+++ to-do_c3/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141117132455) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todo_items", force: true do |t|
     t.string   "todo"
     t.text     "description"
